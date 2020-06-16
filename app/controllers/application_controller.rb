00504b968ed99257e5203d547478df073d96cc0b
Mathieu Dassie <dassie.mathieu@gmail.com>
2020-06-16 09:32:31 +0200

    ajout_champs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nom, :prenom, :nom_societe, :numero_siret, :numero_tva, :adresse, :jours_travail, :heure_debut_travail, :heure_fin_travail])
+  end
 end
+
