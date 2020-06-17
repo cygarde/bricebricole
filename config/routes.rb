f7a4a155fc4c5dda68a1a47092416ed2dbe3fecf
Cyril Garde <cygarde@free.fr>
2020-06-17 22:46:00 +0200

    ajout controller dashboed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clients
   resources :chantiers
+  get '/dashboard', to:'dashboard#dashboard'
 
 end
 
