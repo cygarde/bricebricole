822f03fdda9173eea919fafa76c971e1a804f7e0
Cyril Garde <cygarde@free.fr>
2020-06-20 16:46:00 +0200

    logo & api meteo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :chantiers
+  has_many :taches
+  has_many :clients
 end
