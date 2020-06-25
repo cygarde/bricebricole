f5c3304e122e8f24d7e2c5fd610cb57e81edcd62
Cyril Garde <cygarde@free.fr>
2020-06-25 21:47:52 +0200

    modif-js-homepage

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["clients","agenda", "tâches", "priorités", "devis", "factures", "alertes"],
-    typeSpeed: 70,
+    typeSpeed: 60,
     loop: true
   });
 }
