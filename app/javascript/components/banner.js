a4f38458a914de55f9e66ac22ead3fa27d3148f9
coralie delpha <coralie.delpha@gmail.com>
2020-06-30 08:48:49 +0200

    fix js error typed text on banner

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["clients","agenda", "tâches", "priorités", "devis", "factures", "alertes"],
-    typeSpeed: 60,
-    loop: true
-  });
+  const typedText = document.getElementById("banner-typed-text");
+  if (typedText) {
+    new Typed('#banner-typed-text', {
+      strings: ["clients","agenda", "tâches", "priorités", "devis", "factures", "alertes"],
+      typeSpeed: 60,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
