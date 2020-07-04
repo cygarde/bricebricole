20f074e4d930ef456008f2b8383a1f382dd29c40
coralie delpha <coralie.delpha@gmail.com>
2020-07-04 14:27:36 +0200

    fix taches problem

--- app/models/chantier.rb
+++ app/models/chantier.rb
@@ -2,9 +2,10 @@ class Chantier < ApplicationRecord
   belongs_to :client
   belongs_to :user
   has_many_attached :photos
-  has_many :taches, dependent: :destroy
+  has_many :taches, class_name: 'Tache', dependent: :destroy
 
   include PgSearch::Model
+
   pg_search_scope :search_by_statut,
     against: [ :statut ],
     using: {
