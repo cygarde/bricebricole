d868f6d1f44ec95534fd95a0854406e67412b53c
Cyril Garde <cygarde@free.fr>
2020-07-06 22:31:34 +0200

    fix charge 1/2

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     @chantiersencours = @user.chantiers.where(statut:'encours').count
     @chantiersavenir = @user.chantiers.where(statut:'avenir').count
     @meteodashboard = @user.adresse
-    @taches = Tache.all.where(user_id:@user.id)
+    #@taches = Tache.all.where(user_id:@user.id)
     #@taches = @user.taches.count
     #@tachesarealiser = Tache.where(realisation:false)
 
@@ -14,16 +14,22 @@ class DashboardController < ApplicationController
 
     @from_date = DateTime.now.beginning_of_week
     @to_date = DateTime.now.end_of_week
-    #@tachestotalessemaine = @taches.where("date_debut >= #{@from_date} AND date_debut <= #{@to_date}")
-    @tachestotalessemaine = @user.@taches.where("date_debut >= #{@from_date} AND date_debut <= #{@to_date}")
-    @tempstotaltachestotalesemaine = 0
-    @tachestotalessemaine.each do |tps|
-      dureetotaletache = tps.heure_fin - tps.heure_debut
-      @tempstotaltachestotalesemaine += dureetotaletache
+    @taches_totales_semaine = []
+    @user.chantiers.each do |chantier|
+      chantier.taches.each do |tache|
+        if tache.date_debut >= @from_date && tache.date_debut <= @to_date
+          @taches_totales_semaine << tache
+        end
+      end
     end
-    @tpswjournalier = @user.heure_debut_travail - @user.heure_fin_travail
-    @jourstravaillés = @user.jours_travail.count
-    @tpstotaluser = @jourstravaillés * @tpswjournalier
-    @chargetravailpourcentage = (@tempstotaltachestotalesemaine * 100) / @tpstotaluser
+    @temps_taches_totale_semaine = 0
+    @taches_totales_semaine.each do |tache|
+      @duree_tache = ((tache.heure_fin - tache.heure_debut) / 3600).round
+      @temps_taches_totale_semaine += @duree_tache
+    end
+    #@tps_w_journalier = @user.heure_debut_travail - @user.heure_fin_travail
+    #@jours_travailles = @user.jours_travail.count
+    #@tps_total_user = @jours_travailles * @tpswjournalier
+    #@charge_travail_pourcentage = (@temps_total_taches_totalesemaine * 100) / @tpstotaluser
   end
 end
